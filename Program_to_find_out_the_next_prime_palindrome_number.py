70470058f57a87fc7ec06bfe709d4ef924d3e4fe
Saideepchakkapalli <96737408+Saideepchakkapalli@users.noreply.github.com>
2022-07-10 18:37:03 +0530

    def prime(n):     if n==1:         return 0     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return 0         return 1
    def pal(n):
        rev=0
        while(n):
            rev=rev*10+n%10
            n//=10
        return rev
    
    n = int(input())
    while n:
        n+=1
        x = pal(n)
        if x==n and prime(x):
            print(x)
            break

---
+++ Program_to_find_out_the_next_prime_palindrome_number.py
@@ -0,0 +1,23 @@
+def prime(n):
+    if n==1:
+        return 0
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return 0
+        return 1
+
+def pal(n):
+    rev=0
+    while(n):
+        rev=rev*10+n%10
+        n//=10
+    return rev
+
+n = int(input())
+while n:
+    n+=1
+    x = pal(n)
+    if x==n and prime(x):
+        print(x)
+        break
