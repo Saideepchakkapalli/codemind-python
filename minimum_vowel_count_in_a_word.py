4c82cccf8d969cf3c837d9617c9177265c9254a9
Saideepchakkapalli <96737408+Saideepchakkapalli@users.noreply.github.com>
2022-07-17 21:56:09 +0530

    def min_count(s):     s=s.split()     v=['a','e','i','o','u','A','E','I','O','U']     c=0     for i in range(len(s)):         m=c         c=0         for j in range(len(s[i])):             if s[i][j] in v:                 c+=1         if c>m and m!=0:             c=m     return c s=input() m=min_count(s) print(m)

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,16 @@
+def min_count(s):
+    s=s.split()
+    v=['a','e','i','o','u','A','E','I','O','U']
+    c=0
+    for i in range(len(s)):
+        m=c
+        c=0
+        for j in range(len(s[i])):
+            if s[i][j] in v:
+                c+=1
+        if c>m and m!=0:
+            c=m
+    return c
+s=input()
+m=min_count(s)
+print(m)
