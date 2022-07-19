db2da48a6f019673a3657a336375315c2d429caf
Saideepchakkapalli <96737408+Saideepchakkapalli@users.noreply.github.com>
2022-07-19 20:25:25 +0530

    x,y=map(int,input().split()) l=list(map(int,input().split())) m=list(map(int,input().split())) for i in sorted(set(l),key=l.index):     if m.count(i)==0:         print(i,end=" ") for j in sorted(set(m),key=m.index):     if l.count(j)==0:         print(j,end=" ")

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,10 +1,9 @@
-n,m=map(int,input().split())
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
-l=[]
-for i in sorted(set(a),key=a.index):
-    if b.count(i)==0:
-        print(i,end=' ')
-for j in sorted(set(b),key=b.index):
-    if a.count(j)==0:
-        print(j,end=' ')
+x,y=map(int,input().split())
+l=list(map(int,input().split()))
+m=list(map(int,input().split()))
+for i in sorted(set(l),key=l.index):
+    if m.count(i)==0:
+        print(i,end=" ")
+for j in sorted(set(m),key=m.index):
+    if l.count(j)==0:
+        print(j,end=" ")
